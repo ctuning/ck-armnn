6019168b96a5504b0fd9c3e91ae1ddbd94b7f38e
Leo Gordon <ens-lg4@users.noreply.github.com>
2019-02-13 16:48:13 +0000

    added support for --env.USE_TFLITE=ON

--- package/lib-armnn/scripts.linux/install.sh
+++ package/lib-armnn/scripts.linux/install.sh
@@ -37,21 +37,28 @@ ${ARMNN_SOURCE_DIR}/scripts/generate_tensorflow_protobuf.sh ${TF_PB_DIR} ${CK_EN
 
 ############################################################
 echo ""
-echo "Running cmake for ArmNN with USE_NEON='${USE_NEON}' and USE_OPENCL='${USE_OPENCL}' ..."
+echo "Running cmake for ArmNN with USE_TFLITE='${USE_TFLITE}' and USE_NEON='${USE_NEON}' and USE_OPENCL='${USE_OPENCL}' ..."
 echo ""
 
 if [ "$USE_NEON" == "YES" ] || [ "$USE_NEON" == "yes" ] || [ "$USE_NEON" == "ON" ] || [ "$USE_NEON" == "on" ] || [ "$USE_NEON" == "1" ]
 then
-    NUMERIC_USE_NEON=1
+    EXTRA_CMAKE_FOR_NEON=" -DARMCOMPUTENEON=1 "
 else
-    NUMERIC_USE_NEON=0
+    EXTRA_CMAKE_FOR_NEON=""
 fi
 
 if [ "$USE_OPENCL" == "YES" ] || [ "$USE_OPENCL" == "yes" ] || [ "$USE_OPENCL" == "ON" ] || [ "$USE_OPENCL" == "on" ] || [ "$USE_OPENCL" == "1" ]
 then
-    NUMERIC_USE_OPENCL=1
+    EXTRA_CMAKE_FOR_OPENCL=" -DARMCOMPUTECL=1 "
 else
-    NUMERIC_USE_OPENCL=0
+    EXTRA_CMAKE_FOR_OPENCL=""
+fi
+
+if [ "$USE_TFLITE" == "YES" ] || [ "$USE_TFLITE" == "yes" ] || [ "$USE_TFLITE" == "ON" ] || [ "$USE_TFLITE" == "on" ] || [ "$USE_TFLITE" == "1" ]
+then
+    EXTRA_CMAKE_FOR_TFLITE=" -DBUILD_TF_LITE_PARSER=1 -DTF_LITE_GENERATED_PATH=${CK_ENV_LIB_TF}/src/tensorflow/lite/schema -DFLATBUFFERS_ROOT=${CK_ENV_LIB_FLATBUFFERS} -DFLATBUFFERS_LIBRARY=${CK_ENV_LIB_FLATBUFFERS_LIB}/libflatbuffers.a "
+else
+    EXTRA_CMAKE_FOR_TFLITE=""
 fi
 
 rm -rf "${ARMNN_BUILD_DIR}"
@@ -65,13 +72,12 @@ cmake ${ARMNN_SOURCE_DIR} \
     -DCMAKE_AR="${CK_AR_PATH_FOR_CMAKE}" \
     -DCMAKE_RANLIB="${CK_RANLIB_PATH_FOR_CMAKE}" \
     -DCMAKE_LINKER="${CK_LD_PATH_FOR_CMAKE}" \
-    -DARMCOMPUTENEON=${NUMERIC_USE_NEON} \
-    -DARMCOMPUTECL=${NUMERIC_USE_OPENCL} \
     -DARMCOMPUTE_ROOT=${CK_ENV_LIB_ARMCL} \
     -DARMCOMPUTE_BUILD_DIR=${CK_ENV_LIB_ARMCL}/build \
-    -DBOOST_ROOT=$CK_ENV_LIB_BOOST \
+    -DBOOST_ROOT=${CK_ENV_LIB_BOOST} \
     -DTF_GENERATED_SOURCES=${TF_PB_DIR} \
     -DBUILD_TF_PARSER=1 \
     -DPROTOBUF_ROOT=$CK_ENV_LIB_PROTOBUF_HOST \
+    ${EXTRA_CMAKE_FOR_TFLITE} ${EXTRA_CMAKE_FOR_NEON} ${EXTRA_CMAKE_FOR_OPENCL} \
     -DCMAKE_INSTALL_PREFIX=${ARMNN_TARGET_DIR}
 
