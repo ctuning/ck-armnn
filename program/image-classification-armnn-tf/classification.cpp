03ab727a7418663e39855d20ffbe52d5358faf99
Leo Gordon <ens-lg4@users.noreply.github.com>
2019-03-11 17:18:04 +0000

    adding the experimental program:image-classification-armnn-tf (works with batch_size=1)

---
+++ program/image-classification-armnn-tf/classification.cpp
@@ -0,0 +1,146 @@
+/*
+ * Copyright (c) 2018 cTuning foundation.
+ * See CK COPYRIGHT.txt for copyright details.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause.
+ * See CK LICENSE.txt for licensing details.
+ */
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <memory>
+#include <array>
+#include <algorithm>
+#include <sys/stat.h>
+#include "armnn/ArmNN.hpp"
+#include "armnn/Exceptions.hpp"
+#include "armnn/Tensor.hpp"
+#include "armnn/INetwork.hpp"
+#include "armnnTfParser/ITfParser.hpp"
+
+#include "benchmark.h"
+
+using namespace std;
+using namespace CK;
+
+template <typename TData, typename TInConverter, typename TOutConverter>
+class TFBenchmark : public Benchmark<TData, TInConverter, TOutConverter> {
+public:
+    TFBenchmark(const BenchmarkSettings* settings, TData *in_ptr, TData *out_ptr, int input_index)
+            : Benchmark<TData, TInConverter, TOutConverter>(settings, in_ptr, out_ptr) {
+    }
+};
+
+armnn::InputTensors MakeInputTensors(const std::pair<armnn::LayerBindingId,
+        armnn::TensorInfo>& input, const void* inputTensorData)
+{
+    return { {input.first, armnn::ConstTensor(input.second, inputTensorData) } };
+}
+
+armnn::OutputTensors MakeOutputTensors(const std::pair<armnn::LayerBindingId,
+        armnn::TensorInfo>& output, void* outputTensorData)
+{
+    return { {output.first, armnn::Tensor(output.second, outputTensorData) } };
+}
+
+
+int main(int argc, char* argv[]) {
+    string use_neon             = getenv("USE_NEON");
+    string use_opencl           = getenv("USE_OPENCL");
+    string input_layer_name     = getenv("CK_ENV_TENSORFLOW_MODEL_INPUT_LAYER_NAME");
+    string output_layer_name    = getenv("CK_ENV_TENSORFLOW_MODEL_OUTPUT_LAYER_NAME");
+
+    try {
+        init_benchmark();
+
+        BenchmarkSettings settings;
+
+        // TODO: learn how to process batches via tf.
+        // currently interpreter->tensor(input_index)->dims[0] = 1
+        if (settings.batch_size != 1)
+            throw string("Only BATCH_SIZE=1 is currently supported");
+
+        BenchmarkSession session(&settings);
+
+        unique_ptr<IBenchmark> benchmark;
+        armnnTfParser::ITfParserPtr parser = armnnTfParser::ITfParser::Create();
+        armnn::NetworkId networkIdentifier;
+        armnn::IRuntime::CreationOptions options;
+        armnn::IRuntimePtr runtime = armnn::IRuntime::Create(options);
+        armnn::OutputTensors outputTensor;
+        armnn::InputTensors inputTensor;
+
+        // Optimize the network for a specific runtime compute device, e.g. CpuAcc, GpuAcc
+        //std::vector<armnn::BackendId> optOptions = {armnn::Compute::CpuAcc, armnn::Compute::GpuAcc};
+        std::vector<armnn::BackendId> optOptions = {armnn::Compute::CpuRef};
+        if( (use_neon == "yes") && (use_opencl == "yes") ) {
+            optOptions = {armnn::Compute::CpuAcc, armnn::Compute::GpuAcc};
+        } else if( use_neon == "yes" ) {
+            optOptions = {armnn::Compute::CpuAcc};
+        } else if( use_opencl == "yes" ) {
+            optOptions = {armnn::Compute::GpuAcc};
+        }
+
+        cout << "\nLoading graph..." << endl;
+        measure_setup([&]{
+            armnn::INetworkPtr network = parser->CreateNetworkFromBinaryFile(
+                settings.graph_file.c_str(),
+                { { input_layer_name, {1, 224, 224, 3}} },
+                { output_layer_name }
+            );
+            if (!network)
+                throw "Failed to load graph from file";
+
+            armnnTfParser::BindingPointInfo inputBindingInfo = parser->GetNetworkInputBindingInfo(input_layer_name);
+            armnnTfParser::BindingPointInfo outputBindingInfo = parser->GetNetworkOutputBindingInfo(output_layer_name);
+
+            armnn::TensorShape inShape = inputBindingInfo.second.GetShape();
+            armnn::TensorShape outShape = outputBindingInfo.second.GetShape();
+            std::size_t inSize = inShape[0] * inShape[1] * inShape[2] * inShape[3];
+            std::size_t outSize = outShape[0] * outShape[1];
+
+            armnn::IOptimizedNetworkPtr optNet = armnn::Optimize(*network, optOptions, runtime->GetDeviceSpec());
+
+            runtime->LoadNetwork(networkIdentifier, std::move(optNet));
+            float *output = new float[outSize];
+            float *input = new float[inSize];
+
+            outputTensor = MakeOutputTensors(outputBindingInfo, output);
+            inputTensor = MakeInputTensors(inputBindingInfo, input);
+
+            benchmark.reset(new TFBenchmark<float, InNormalize, OutCopy>(&settings, input, output, 0));
+
+            int out_num = outShape[0];
+            int out_classes = outShape[1];
+            cout << format("Output tensor dimensions: %d*%d", out_num, out_classes) << endl;
+            if (out_classes != settings.num_classes && out_classes != settings.num_classes+1)
+                throw format("Unsupported number of classes in graph's output tensor. Supported numbers are %d and %d",
+                             settings.num_classes, settings.num_classes+1);
+            benchmark->has_background_class = out_classes == settings.num_classes+1;
+        });
+
+        cout << "\nProcessing batches..." << endl;
+        measure_prediction([&]{
+            while (session.get_next_batch()) {
+                session.measure_begin();
+                benchmark->load_images(session.batch_files());
+                session.measure_end_load_images();
+
+                session.measure_begin();
+                if (runtime->EnqueueWorkload(networkIdentifier, inputTensor, outputTensor) != armnn::Status::Success)
+                    throw "Failed to invoke tf";
+                session.measure_end_prediction();
+
+                benchmark->save_results(session.batch_files());
+            }
+        });
+
+        finish_benchmark(session);
+    }
+    catch (const string& error_message) {
+        cerr << "ERROR: " << error_message << endl;
+        return -1;
+    }
+    return 0;
+}
---
+++ program/image-classification-armnn-tf/preprocess-next.py
@@ -0,0 +1,39 @@
+#
+# Copyright (c) 2018 cTuning foundation.
+# See CK COPYRIGHT.txt for copyright details.
+#
+# SPDX-License-Identifier: BSD-3-Clause.
+# See CK LICENSE.txt for licensing details.
+#
+
+import os
+
+def ck_preprocess(i):
+  def dep_env(dep, var): return i['deps'][dep]['dict']['env'].get(var)
+  
+  MODEL_FILE = ''
+  MODEL_DIR = dep_env('weights', 'CK_ENV_TENSORFLOW_MODEL_ROOT')
+  for filename in os.listdir(MODEL_DIR):
+    if filename.endswith('.pb'):
+      MODEL_FILE = filename
+      MODEL_PATH = os.path.join(MODEL_DIR, MODEL_FILE)
+  if not MODEL_FILE:
+    return {'return': 1, 'error': 'Tf graph is not found in the selected model package'}
+
+  # Setup parameters for program
+  new_env = {}
+  files_to_push_by_path = {}
+
+  if i['target_os_dict'].get('remote','') == 'yes':
+    # For Android we need only filename without full path  
+    new_env['RUN_OPT_GRAPH_FILE'] = MODEL_FILE
+    files_to_push_by_path['RUN_OPT_GRAPH_PATH'] = MODEL_PATH
+  else:
+    new_env['RUN_OPT_GRAPH_FILE'] = MODEL_PATH
+
+  print('--------------------------------\n')
+  return {
+    'return': 0,
+    'new_env': new_env,
+    'files_to_push_by_path': files_to_push_by_path,
+  }
