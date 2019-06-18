1785ab68f9953dabe40169f25170faae48e593fa
Leo Gordon <ens-lg4@users.noreply.github.com>
2019-06-18 18:52:35 +0100

    added TF/TFlite/ONNX parser support to Android version

---
+++ package/lib-armnn/scripts.android/post-install2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#
+# See CK LICENSE.txt for licensing details.
+# See CK COPYRIGHT.txt for copyright details.
+#
+# Developer(s):
+# - Anton Lokhmotov, 2019
+#
+
+function exit_if_error() {
+    if [ "${?}" != "0" ]; then exit 1; fi
+}
+
+# A workaround for an ArmNN v19.05 issue: https://github.com/ARM-software/armnn/issues/196
+if [ "$USE_TFLITE" == "YES" ] && [ "$PACKAGE_VERSION" == "19.05" ]
+then
+    echo ""
+    echo "Copying 'libarmnnTfLiteParser.so' to '${INSTALL_DIR}/install/lib' ..."
+    cp ${INSTALL_DIR}/obj/libarmnnTfLiteParser.so ${INSTALL_DIR}/install/lib/
+    exit_if_error
+fi
